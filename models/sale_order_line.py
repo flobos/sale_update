7d759a96e8d2cb1a31479d83f1d4acc0ef2528b9
flobos <fernandolobos@gmail.com>
2019-12-11 12:06:20 -0300

    sale order line stock

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -4,6 +4,8 @@ class sale_update_sale_order_line(models.Model):
 
     _inherit = 'sale.order.line'
     marge_porcentaje = fields.Float(string="Margen (%)", readonly=True, store=True, compute='_calcular_margen')
+    stock_disponible = fields.Float(string="Stock venta", readonly=True, store=True, compute='_calcular_stock')
+
 
     @api.multi
     @api.depends('product_id','price_unit')
@@ -13,6 +15,17 @@ class sale_update_sale_order_line(models.Model):
             if rec.price_unit != 0:
                 margen = ((rec.price_unit - rec.purchase_price) / rec.price_unit) * 100
                 rec.marge_porcentaje = margen
+
+    @api.multi
+    @api.depends('product_id')
+    def _calcular_stock(self):
+        for rec in self:
+            rec.stock_disponible = rec.product_id.product_tmpl_id.productos_disponible
+
+
+
+
+
 
 
 
